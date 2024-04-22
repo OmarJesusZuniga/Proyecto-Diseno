75bfb269e5ab01e161c0a764776f1f31878d1685
RichiReyes <reyesrichi05@gmail.com>
2024-04-22 16:47:36 -0600

    proxy server para traer data del backend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,11 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+    
+    useEffect(() => {
+
+    }, [])
+    
     return (
         <div className="home"> 
         <h2>Home</h2>
