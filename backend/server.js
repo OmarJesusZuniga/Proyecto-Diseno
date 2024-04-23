b46875d9f0184c9853070290cf40b7beb8f35963
AdrianVega1 <advega@estudiantec.cr>
2024-04-23 12:00:34 -0600

    Student

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
-const workoutRoutes = require('./routes/workouts')
-const testRoutes = require('./routes/testroutes')
+const studentRoutes = require('./routes/studentroutes')
 
 // express app
 const app = express()
@@ -17,9 +16,8 @@ app.use((req, res, next) => {
 })
 
 // routes
-app.use('/api/workouts', workoutRoutes)
 
-app.use('/tests/', testRoutes)
+app.use('/students', studentRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
