f80c8cafdfa461e283b493f40935b9bc57aa2b9c
OmarJesusZuniga <omzuniga@estudiantec.cr>
2024-04-22 18:18:09 -0600

    Update

---
+++ backend/models/guideProfessorModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const professorSchema = new Schema ({
+    profesor: [{ 
+        type: mongoose.Schema.Types.ObjectId, // Id a sus atributos de profesor
+        ref: 'professor'
+    }]
+})
+
+module.exports = mongoose.model('guideProfessor', professorSchema)
