840692189bec83beecf55d20feca10145ada829c
jossanchez04 <jos.sanchez@estudiantec.cr>
2024-04-24 22:07:08 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route, Router} from 'react-router-dom'
 
 //pages & components
 import Home from './pages/Home';
+import HomeAdmin from './pages/HomeAdmin';
 import Navbar from './components/Navbar';
 import LoginForm from './pages/LoginForm';
 
@@ -11,7 +12,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route exact path="/" element={<LoginForm/>} />
-          <Route exact path="/home" element={<Home/>}/>
+          <Route exact path="/home" element={<HomeAdmin/>}/>
         </Routes>
       </div>
     </BrowserRouter>
