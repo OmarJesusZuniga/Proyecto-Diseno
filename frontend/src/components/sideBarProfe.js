2e32e1325744b94baa554c246e4e79d2b4c6dd4f
RichiReyes <reyesrichi05@gmail.com>
2024-04-30 16:07:47 -0600

    cambios para home de profe

--- frontend/src/components/sideBarProfe.js
+++ frontend/src/components/sideBarProfe.js
@@ -2,7 +2,31 @@ import React from 'react';
 import '../components/sideBar.css';
 
 
-const SideBarProfe = () => {
+const SideBarProfe = ({sA, sE, sB, sP}) => {
+
+    const dejarProfes = () => {
+        sB(false);
+        sA(false);
+        sE(false);
+        sP(true);
+    }
+
+    const dejarEstudiantes = () => {
+        sB(false);
+        sA(false);
+        sP(false);
+        sE(true);
+    }
+
+    const dejarActividades = () => {
+        sB(false);
+        sP(false);
+        sE(false);
+        sA(true);
+    }
+
+
+
     return (
         <div className='sidebar'>
             <ul>
@@ -10,9 +34,27 @@ const SideBarProfe = () => {
                     <div className="dropdown">
                         <h2>Equipo Guía</h2>
                         <select>
-                            <option value="team1">Equipo guía primer ingreso</option>
+                            <option  value="team1">Equipo guía primer ingreso</option>
                         </select>
                     </div>
+                </li>
+                <li>
+                    <div className="section">
+                        <h2>Profesores de equipo guía</h2>
+                        <button onClick={dejarProfes}>Ver lista</button>
+                    </div>
+                </li>
+                <li>
+                   <div className="section">
+                       <h2>Estudiantes</h2>
+                       <button onClick={dejarEstudiantes} className="botonAzul">Ver lista</button>
+                   </div>
+                </li>
+                <li>
+                   <div className="section">
+                       <h2>Actividades</h2>
+                       <button onClick={dejarActividades} className="botonAzul">Ver actividades</button>
+                   </div>
                 </li>
             </ul>
         </div>
--- frontend/src/pages/HomeProfe.js
+++ frontend/src/pages/HomeProfe.js
@@ -1,22 +1,29 @@
 import Navbar from "../components/Navbar";
 import { useLocation } from "react-router-dom";
-import React from 'react';
+import React, { useState } from 'react';
 import SideBarProfe from "../components/sideBarProfe";
 
 const HomeProfe = () => {
     const {state} = useLocation();
     const {usuario} = state || {};
-    
+
+    const [listaProfes, setProfes] = useState(true)
+    const [listaActividades, setActividades] = useState(false)
+    const [listaEstudiantes, setEstudiantes] = useState(false)
+    const [bienvenida, setBienvenida] = useState(true);
     
     
     
     return (
-        <div className="home">
+        <div className="home"> 
             <Navbar  id={usuario.firstname} apellido={usuario.firstLastname}/>
             <div className="horizontal-container">
-                <SideBarProfe/>
-                <div className="contenedor lista">
-                    
+                <SideBarProfe sP = {setProfes} sA = {setActividades} sE={setEstudiantes} sB ={setBienvenida}/>
+                <div className="contenedorListas">
+                    {bienvenida && <div className="contenido"><h1>Bienvenido, profesor</h1></div>}
+                    {}
+                    {}
+                    {}
                 </div>
 
             </div>
