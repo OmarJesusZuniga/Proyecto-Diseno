b0eb934b2a7e49353cf3932816f7b0f7b3698610
jossanchez04 <jos.sanchez@estudiantec.cr>
2024-05-06 18:46:53 -0600

    Update HomeProfe.js

--- frontend/src/pages/HomeProfe.js
+++ frontend/src/pages/HomeProfe.js
@@ -11,8 +11,8 @@ const HomeProfe = () => {
     const {usuario} = state || {};
 
     const [listaProfes, setProfes] = useState(true)
-    const [listaActividades, setActividades] = useState(false)
-    const [ListaObservaciones, setObservaciones] = useState(false)
+    const [listaActividadesProfe, setActividadesProfe] = useState(false)
+    const [listaObservaciones, setObservaciones] = useState(false)
     const [listaEstudiantes, setEstudiantes] = useState(false)
     const [bienvenida, setBienvenida] = useState(true);
     
@@ -22,12 +22,12 @@ const HomeProfe = () => {
         <div className="home"> 
             <Navbar  id={usuario.firstname} apellido={usuario.firstLastname}/>
             <div className="horizontal-container">
-                <SideBarProfe usuario={usuario} sP = {setProfes} sA = {setActividades} sO = {setObservaciones} sE={setEstudiantes} sB ={setBienvenida} id={usuario._id} grupo={setGrupo}/>
+                <SideBarProfe usuario={usuario} sP = {setProfes} sA = {setActividadesProfe} sO = {setObservaciones} sE={setEstudiantes} sB ={setBienvenida} id={usuario._id} grupo={setGrupo}/>
                 <div className="contenedorListas">
                     {bienvenida && <div className="contenido"><h1>Bienvenido, profesor</h1></div>}
                     {listaEstudiantes && <ListaEstudiantesProfe campus={usuario.campus}/> }
-                    {ListaObservaciones && <ListaObservaciones usuario={usuario} campus={usuario.campus}/>}
-                    {listaActividades && <ListaActividadesProfe grupo={grupo} usuario={usuario} />}
+                    {listaObservaciones && <ListaObservaciones usuario={usuario} campus={usuario.campus}/>}
+                    {listaActividadesProfe && <ListaActividadesProfe grupo={grupo} usuario={usuario} />}
                 </div>
 
             </div>
