822129b535e6345f41ca753f28a011db30e676b3
OmarJesusZuniga <omzuniga@estudiantec.cr>
2024-05-13 21:33:11 -0600

    Revert "a"
    This reverts commit 899ebec92bc6b503080e84c727fa2748d1f21c69.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-
-if (process.env.NODE_ENV === 'production') {
-  disableReactDevTools();
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,3 +9,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
