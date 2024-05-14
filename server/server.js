dd81da05738819fcc4ff4738b2ce397503c0b592
OmarJesusZuniga <omzuniga@estudiantec.cr>
2024-05-13 20:33:01 -0600

    uPDATE

--- server/server.js
+++ server/server.js
@@ -65,7 +65,9 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('connected to database')
     // listen to port
-
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
   })
   .catch((err) => {
     console.log(err)
