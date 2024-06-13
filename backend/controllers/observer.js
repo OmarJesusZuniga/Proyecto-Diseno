49f51759aed4933701b09262ba419ba9daf1c316
OmarJesusZuniga <omzuniga@estudiantec.cr>
2024-06-13 16:54:31 -0600

    Update - Observer / Visitor

--- backend/controllers/observer.js
+++ backend/controllers/observer.js
@@ -1,42 +1,57 @@
 const Activity = require('../models/activityModel')
 const ActivityState = require('../models/activityStateModel')
+const { ConcreteVisitor } = require('../controllers/visitor')
 const mongoose = require('mongoose')
 
-// Get all
-const notify = async (req, res) => {
-    const activities = await Activity.find({}).sort({createdAt: 1});
+class Observer {
+    constructor() {
+        this.concreteVisitor = new ConcreteVisitor();
+    }
 
-    const currentDate = new Date();
-    currentDate.setDate(currentDate.getDate() - 1); 
+    async notify() {
+        const activities = await Activity.find({}).sort({createdAt: 1});
 
-    for (let i = 0; i < activities.length; i++) {
-        const publicationDate = activities[i].publishDate; // Fecha de Publicacion
+        const currentDate = new Date();
+        currentDate.setDate(currentDate.getDate() - 1); 
 
-        const element = activities[i];
-        const stateId = element.state.toString();
-        const state = await ActivityState.findById(stateId);
+        for (let i = 0; i < activities.length; i++) {
+            const publicationDate = activities[i].publishDate; // Fecha de Publicacion
 
-        if (state && (state.type === 'Planeada') && (currentDate >= publicationDate)) {
-            // Llamar a Visitor para enviar la notificacion [ANUNCIO DE ACTIVIDAD]
+            const element = activities[i];
+            const stateId = element.state.toString();
+            const state = await ActivityState.findById(stateId);
 
-            // Cambiar estado a Notificada
-        }
+            if (state && (state.type === 'Planeada') && (currentDate >= publicationDate)) {
+                // Llamar a Visitor para enviar la notificacion [ANUNCIO DE ACTIVIDAD]
+                this.concreteVisitor.visitAnouncement(element); 
+                // Cambiar estado a Notificada
+                // element.state = "Notificada";
+                // await element.save();
+            }
 
-        if (state && (state.type === 'Notificada')) {
-            for (let j = 0; j < element.reminders.length; j++) {
-                const reminderDate = element.reminders[j];
+            if (state && (state.type === 'Notificada')) {
+                for (let j = 0; j < element.reminders.length; j++) {
+                    const reminderDate = element.reminders[j];
 
-                if (reminderDate <= currentDate) {
-                    // Llamar a Visitor para enviar la notificacion [RECORDATORIO DE ACTIVIDAD]
+                    if (reminderDate >= currentDate) {
+                        // Llamar a Visitor para enviar la notificacion [RECORDATORIO DE ACTIVIDAD]
+                        this.concreteVisitor.visitReminder(element);
 
-                    // Eliminar el recordatorio de la lista
+                        // Eliminar el recordatorio de la lista
+                        element.reminders.splice(j, 1);
+                        j--;
+                    }
                 }
+
+                // if (element.isModified('reminders')) {
+                //     await element.save();
+                // }
             }
         }
     }
+
 }
 
-
 module.exports = {
-    notify
+    Observer
 }
